615dad2cf4dde8117f8319ecc07c1696533c9f2e
Michael <michael@managedkaos.com>
2018-09-16 16:34:23 -0700

    Update outputs

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ from ftplib import FTP
 
 def lambda_handler(event, context):
     # read the config file and assign values
+    print("### Read the config file and assign values")
     config = ConfigParser()
     config.read('config.txt')
 
@@ -12,21 +13,26 @@ def lambda_handler(event, context):
     ftp_host   = config.get('ftp','host')
     ftp_user   = config.get('ftp','user')
     ftp_pass   = config.get('ftp','pass')
-    local_dir  = config.get('local','dir')
+    work_dir  = config.get('local','dir')
 
-    # check for the local directory and create it if needed
-    if not os.path.exists(local_dir):
-        os.makedirs(local_dir)
+    # check for the working directory and create it if needed
+    print("### Check for the local directory and create it if needed")
+    if not os.path.exists(work_dir):
+        os.makedirs(work_dir)
 
-    # change directory to the local directory
-    os.chdir(local_dir)
+    # change directory to the working directory
+    print("### Change to the local directory")
+    os.chdir(work_dir)
 
     # remove the column bounds
     pandas.set_option('display.expand_frame_repr', False)
+
     # get the data or die
+    print("### Get the data from Google Sheets")
     p = pandas.read_csv(gsheet_url)
 
     # open ftp or die
+    print("### Open a connection to the FTP host")
     ftp = FTP(ftp_host)
     ftp.login(ftp_user, ftp_pass)
 
@@ -50,21 +56,23 @@ def lambda_handler(event, context):
     ]
 
     # simplify the column names
+    print("### Simplify the column names")
     p.rename(columns={'Phone Number (in xxx-xxx-xxxx format)':'Phone Number'}, inplace=True)
     p.rename(columns={'Target Committees (select at least one)':'Target Committees'}, inplace=True)
 
     # drop duplicates by email address
+    print("### Drop duplicates by email address")
     p.drop_duplicates('Preferred Email Address', inplace=True)
 
-    # print all member details, sorted by last name
-    print("All members...")
+    # process all member details, sorted by last name
+    print("### Process all member details, sorted by last name")
     p.iloc[:, 1:9].sort_values(by=['Last Name'], ascending=True).to_csv('members.csv')
     p.iloc[:, 1:9].sort_values(by=['Last Name'], ascending=True).to_html('members.html')
     ftp.storbinary('STOR ' + 'members.html', open ('members.html', 'rb'))
     ftp.storbinary('STOR ' + 'members.csv', open ('members.csv', 'rb'))
 
-    # print target committees with member details
-    print("Target committees...")
+    # process target committees with member details
+    print("### Process target committees with member details")
     for committee in target_committees:
         committee_filename = ''.join(e for e in committee if e.isalnum())
         pslice = p[ p['Target Committees'].str.contains(committee, na=False) ]
